4d78fec75426ed3200ca54955b371a2688b2e7ff
Marcus <marcusadrenaline@gmail.com>
2022-04-16 11:08:32 -0300

    funcão de verificar o nome corrigida

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let container = document.querySelector(".container");
-let cadastro = prompt("Qual é seu nome? ")
+let cadastro = prompt("Qual é seu nome? ");
+let verificar_nome = false;
+let usuario;
 
 function login(){
 
     usuario = {
         name: cadastro
     };
-    
     const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', usuario);
+
+
+    requisicao.catch(function (){
+        alert("Nome ja em uso! tente outro.");
+        location. reload();
+    })
+
+
+
+
+
+
+    
+   
     const usuarioativo = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', usuario);
     
     let mantercoenxao = setInterval(manterConexao,5000);
@@ -39,14 +54,14 @@ function buscarMensagens(){
         console.log(dados);
         container.innerHTML = "";
         for (i = 0; i < dados.length; i ++){
-           if (dados[i].type === 'status'){
-            container.innerHTML += 
-            `<div class="entrada ${dados[i].type}">
-                <span class="horario"><h1 horário>(${dados[i].time})</h1>
-                </span>
-                <span><h1><strong>${dados[i].from} </strong>${dados[i].text}</h1></span>
-            </div>`
-           } else if (dados[i].type === 'private_message'){
+            if (dados[i].type === 'status'){
+                container.innerHTML += 
+                `<div class="entrada ${dados[i].type}">
+                    <span class="horario"><h1 horário>(${dados[i].time})</h1>
+                    </span>
+                    <span><h1><strong>${dados[i].from} </strong>${dados[i].text}</h1></span>
+                </div>`
+            } else if (dados[i].type === 'private_message'){
                // Verifica se a mensagem privada é minha ou pra mim.
                 if (dados[i].from === cadastro || dados[i].to === cadastro){
                     container.innerHTML += 
@@ -56,19 +71,25 @@ function buscarMensagens(){
                         <span><h1><strong>${dados[i].from} </strong> reservadamente para <strong>${dados[i].to}: </strong>${dados[i].text}</h1></span>
                     </div>`
                 }
-           } else if (dados[i].type === 'message') {
+            } else if (dados[i].type === 'message') {
             container.innerHTML += 
             `<div class="entrada ${dados[i].type}">
                 <span class="horario"><h1 horário>(${dados[i].time})</h1>
                 </span>
                 <span><h1><strong>${dados[i].from} </strong>para <strong>${dados[i].to}: </strong>${dados[i].text}</h1></span>
             </div>`
-           }
-           ultimamsg = document.querySelector('.container div:last-child');
-            if ((dados[dados.length-1].from != dados[dados.length-2].from) && (dados[dados.length-1].text != dados[dados.length-2].text)){
-                ultimamsg.scrollIntoView();
             }
-        ultimamsg = 0;
+            if ((dados[dados.length-1].type == 'message') || (dados[dados.length-1].type == 'private-message')){
+                ultimamsg = document.querySelector('.container div:last-child');
+                let verifica_message = []
+
+                verifica_message.push(ultimamsg).from;
+
+                if ((dados[dados.length-1].from != dados[dados.length-2].from) && (dados[dados.length-1].text != dados[dados.length-2].text)){
+                    ultimamsg.scrollIntoView();
+                    ultimamsg = 0;
+                }
+            }
         }
     }
 }
@@ -97,5 +118,5 @@ function enviarMensagem(){
     })
 }
 
-setInterval(buscarMensagens,5000);
+setInterval(buscarMensagens,3000);
 login();
